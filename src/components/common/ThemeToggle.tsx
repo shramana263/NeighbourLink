cede7759caf4a5e7b8e886dd669398be9e487c5d
The-Parthib <rijupanja81@gmail.com>
2025-04-18 22:53:43 +0530

    theme toggler

--- src/components/common/ThemeToggle.tsx
+++ src/components/common/ThemeToggle.tsx
@@ -10,12 +10,19 @@ const ThemeToggle: React.FC = () => {
     <>
       {
         user ?
-          <button
-            onClick={toggleTheme}
-            className="p-2 rounded w-full bg-transparent items-center flex justify-center"
-          >
-            {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
-          </button>
+            <div className="flex items-center space-x-2">
+            <span>{theme === 'dark' ? 'Dark Mode' : 'Light Mode'}</span>
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input
+              type="checkbox"
+              className="sr-only peer"
+              checked={theme === 'dark'}
+              onChange={toggleTheme}
+              />
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:bg-gray-700 peer-checked:bg-blue-600 rounded-full peer dark:peer-focus:ring-blue-800"></div>
+              <span className="absolute left-1 top-1 w-4 h-4 bg-gray-500 dark:bg-green-500 rounded-full transition-transform peer-checked:translate-x-5"></span>
+            </label>
+            </div>
           :
           <button
             onClick={toggleTheme}
