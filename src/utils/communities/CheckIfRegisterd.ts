d2241dfcaa3be9e27f33bf7b436483f0a8f040a7
Mrinmoy Mondal <mrinmoymondalreal+github@gmail.com>
2025-04-19 01:19:47 +0530

    add function to check if user registered in community

---
+++ src/utils/communities/CheckIfRegisterd.ts
@@ -0,0 +1,23 @@
+import { useStateContext } from "@/contexts/StateContext";
+import { db } from "@/firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
+
+export const isUserRegistered = async (
+  email: string,
+  community: string
+): Promise<boolean> => {
+  const q = query(collection(db, community), where("email", "==", email));
+
+  const querySnapshot = await getDocs(q);
+
+  if (!querySnapshot.empty) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const checkIfUserRegisteredInSkillSharing = async () => {
+  const { user } = useStateContext();
+  return await isUserRegistered(user?.email as string, "skill-sharing");
+};
